a7b1d41d03055a58d54bf05e3b0b3c56c0aa04ec
Rene <renealejandrosanchezmorales@gmail.com>
2021-05-29 00:10:15 -0400

    plus to challenge2 to printed status bmi to wight

--- day2/challenge2.py
+++ day2/challenge2.py
@@ -1,8 +1,19 @@
 
 
-def calculate_ibm(height, weight):
+def status_bmi(bmi):
+    if bmi < 18.5:
+        print("Estas Bajo Peso")
+    elif bmi > 18.5 and bmi < 25:
+        print("Tienes Peso Normal")
+    elif bmi > 25 and bmi < 30:
+        print("Estas Sobre Peso")
+    else:
+        print("Tienes Obesidad")
+
+def calculate_bmi(height, weight):
 
     result = weight / (height ** 2)
+    status_bmi(result)
     result = int(result)
 
     return result
@@ -10,8 +21,8 @@ def calculate_ibm(height, weight):
 def run():
     height = input("enter your height in m: ")
     weight = input("enter your weight in kg: ")
-    ibm = calculate_ibm(float(height), int(weight))
-    print(ibm)
+    bmi = calculate_bmi(float(height), int(weight))
+    print(bmi)
 
 if __name__ == '__main__':
     run()
