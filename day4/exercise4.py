6680084e9c50d0310888cfdf7c60164b347f2866
Rene <renealejandrosanchezmorales@gmail.com>
2021-06-05 22:49:45 -0400

    performance game exercise4

--- day4/exercise4.py
+++ day4/exercise4.py
@@ -27,53 +27,41 @@ scissors = '''
 ---.__(___)
 '''
 
-dict_choosers = {
-    "0":rock,
-    "1": paper,
-    "2": scissors
-}
+games_images = [rock, paper, scissors]
 
 def winner_choose(human, computer):
 
     is_winner = False
     is_tie = False
 
-    if computer == 2 and human == 1:
-        is_winner = False
-    elif computer == 2 and human == 0:
-        is_winner = True
-    elif computer == 2 and human == 2:
-        is_tie = True
-    elif computer == 1 and human == 1:
-        is_tie = True
-    elif computer == 1 and human == 2:
-        is_winner = True
-    elif computer == 1 and human == 0:
-        is_winner = False
-    elif computer == 0 and human == 0:
-        is_tie = True
-    elif computer == 0 and human == 1:
-        is_winner = True
-    elif computer == 0 and human == 2:
-        is_winner = False
-
-    if is_tie:
-        print("Tie")
-    elif is_winner:
-        print("You win")
+    if human >= 3 or human < 0:
+        print("You typed an invaid number, you lose!")
     else:
-        print("You lose")
+        if human == 0 and computer == 2:
+            is_winner = True
+        elif computer == 0 and human == 2:
+            is_winner = False
+        elif computer > human:
+            is_winner = False
+        elif human > computer:
+            is_winner = True
+        elif human == computer:
+            is_tie = True
+
+        if is_tie:
+            print("It's a draw")
+        elif is_winner:
+            print("You win")
+        else:
+            print("You lose")
 
 def run():
     choose = int(input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
     choose_random = random.randint(0, 2)
     print("\n")
-    print(dict_choosers[str(choose)])
-    print(f"\nComputer chose:\n{dict_choosers[str(choose_random)]}")
+    print(games_images[choose])
+    print(f"\nComputer chose:\n{games_images[choose_random]}")
     winner_choose(choose, choose_random)
-
-
-
 
 if __name__ == "__main__":
     run()
