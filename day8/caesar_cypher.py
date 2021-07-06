edeea0bdddc4abff92088a3dcc4e895e1816e45a
Rene <renealejandrosanchezmorales@gmail.com>
2021-07-05 21:56:53 -0400

    caesar encrypt and decrypt

---
+++ day8/caesar_cypher.py
@@ -0,0 +1,21 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet += ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+limit_position = len(alphabet)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    
+    for letter in start_text:
+        position = alphabet.index(letter)
+        position += shift_amount
+        end_text += alphabet[position]
+    
+    print(f"Here's the {cipher_direction} result: {end_text}")
+
+caesar(text, shift, direction)
