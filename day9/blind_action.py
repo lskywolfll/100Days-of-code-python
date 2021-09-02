d99cb9d9ee6043594e1158fa56c52ff817e376b0
Rene <renealejandrosanchezmorales@gmail.com>
2021-09-01 21:11:52 -0400

    added art ascci for blind_actiond when use bids

---
+++ day9/blind_action.py
@@ -0,0 +1,38 @@
+from art import logo
+from os import system
+
+
+def clear_console():
+    system('clear')
+
+
+def find_highest_bidder(bidding_record: dict):
+    highest_bid = 0
+    winner = ""
+
+    for name,price in bidding_record.items():
+        if highest_bid < price:
+            highest_bid = price
+            winner = name
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+
+print(logo)
+
+bids = {}
+
+bidding_finished = False
+
+while not bidding_finished:
+    name = input('What is your name?: ')
+    price = int(input("What's your bid?: "))
+    bids[name] = price
+    should_continue = input(
+        "Are there any other bidders? Type 'yes' or 'no': ")
+    if should_continue == 'yes':
+        clear_console()
+    else:
+        clear_console()
+        find_highest_bidder(bids)
+        bidding_finished = True
